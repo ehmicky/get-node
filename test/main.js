fab23e6a80d02eca2b7511ffeca0f55541ae27c1
ehmicky <ehmicky@gmail.com>
2019-08-10 10:00:00 +0200

    Add test

--- test/main.js
+++ test/main.js
@@ -1,5 +1,26 @@
-import test from 'ava'
+import { tmpdir } from 'os'
+import { promisify } from 'util'
+import { unlink, rmdir } from 'fs'
+import { resolve } from 'path'
 
-test('Dummy test', t => {
-  t.pass()
+import test from 'ava'
+import pathExists from 'path-exists'
+
+import getNode from '../src/main.js'
+
+const pUnlink = promisify(unlink)
+const pRmdir = promisify(rmdir)
+
+test('Success', async t => {
+  const id = String(Math.random()).replace('.', '')
+  const outputDir = `${tmpdir()}/test-get-node-${id}`
+  const versionRange = '6.0.0'
+
+  const nodePath = await getNode(versionRange, outputDir)
+
+  t.true(await pathExists(nodePath))
+
+  await pUnlink(nodePath)
+  await pRmdir(resolve(nodePath, '..'))
+  await pRmdir(resolve(nodePath, '../..'))
 })

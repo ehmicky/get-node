1a401cbad5cc8ca6d1c675a89b3ca1f02738ef2b
ehmicky <ehmicky@gmail.com>
2019-11-03 15:07:46 +0100

    Add tests

--- test/download.js
+++ test/download.js
@@ -4,6 +4,7 @@ import { unlink } from 'fs'
 import { resolve } from 'path'
 
 import test from 'ava'
+import { each } from 'test-each'
 import pathExists from 'path-exists'
 import execa from 'execa'
 import pathKey from 'path-key'
@@ -17,7 +18,7 @@ import {
   getNodePath,
 } from './helpers/main.js'
 // eslint-disable-next-line import/max-dependencies
-import { TEST_VERSION } from './helpers/versions.js'
+import { NO_XZ_VERSION, TEST_VERSION } from './helpers/versions.js'
 
 const ATOMIC_PROCESS = `${__dirname}/helpers/atomic.js`
 
@@ -69,18 +70,25 @@ test('Writes atomically', async t => {
   t.false(await pathExists(resolve(path, '../..')))
 })
 
-test('HTTP errors', async t => {
-  // Ensure normalize-node-version is cached
-  await getNode(TEST_VERSION)
+each(
+  [
+    { mirror: 'https://unknown-mirror.com', message: /Could not connect/u },
+    // We cannot test this since all the OS/architecture tested in CI have
+    // Node.js binaries. So we simulate it by using a `mirror` to an existing
+    // website, leading to a 404.
+    { mirror: 'https://example.com', message: /No Node\.js binaries/u },
+  ],
+  [NO_XZ_VERSION, TEST_VERSION],
+  ({ title }, { mirror, message }, version) => {
+    test(`HTTP error | ${title}`, async t => {
+      // Ensure normalize-node-version is cached
+      await getNode(version)
 
-  const output = getOutput()
-  await t.throwsAsync(
-    getNode(TEST_VERSION, { output, mirror: INVALID_MIRROR }),
-    /Could not download/u,
-  )
-})
-
-const INVALID_MIRROR = 'https://example.com'
+      const output = getOutput()
+      await t.throwsAsync(getNode(version, { output, mirror }), message)
+    })
+  },
+)
 
 test.serial('Works when no xz binary exists', async t => {
   const pathEnv = env[PATH_KEY]

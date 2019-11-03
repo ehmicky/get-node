1150e4ffe2d5ae43733c8f2e8cce5cb63756d659
ehmicky <ehmicky@gmail.com>
2019-11-03 11:53:21 +0100

    Add test

--- test/main.js
+++ test/main.js
@@ -1,7 +1,10 @@
+import { env } from 'process'
+
 import test from 'ava'
 import pathExists from 'path-exists'
 import execa from 'execa'
 import { each } from 'test-each'
+import pathKey from 'path-key'
 
 import getNode from '../src/main.js'
 
@@ -28,3 +31,21 @@ each(
     })
   },
 )
+
+test.serial('Works when no xz binary exists', async t => {
+  const pathEnv = env[PATH_KEY]
+  // eslint-disable-next-line fp/no-mutation
+  env[PATH_KEY] = ''
+
+  const output = getOutput()
+  const { path } = await getNode(TEST_VERSION, { output })
+
+  t.true(await pathExists(path))
+
+  await removeOutput(path)
+
+  // eslint-disable-next-line fp/no-mutation, require-atomic-updates
+  env[PATH_KEY] = pathEnv
+})
+
+const PATH_KEY = pathKey()

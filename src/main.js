9219b386550593538dc69c887c04d2317cad5ff8
ehmicky <ehmicky@gmail.com>
2020-04-19 11:07:54 +0200

    Move file

--- src/main.js
+++ src/main.js
@@ -1,11 +1,6 @@
-import { version as processVersion } from 'process'
-
-import nodeVersionAlias from 'node-version-alias'
-import preferredNodeVersion from 'preferred-node-version'
-import { lt as ltVersion } from 'semver'
-
 import { download } from './download.js'
 import { getOpts } from './options.js'
+import { getVersion } from './version.js'
 
 // Download the Node.js binary for a specific `versionRange`
 const getNode = async function (versionRange, opts) {
@@ -23,41 +18,9 @@ const getNode = async function (versionRange, opts) {
     preferredNodeOpts,
     nodeVersionAliasOpts,
   })
-  checkVersion(version)
-
   const nodePath = await download({ version, output, arch, fetchOpts })
   return { version, path: nodePath }
 }
-
-const getVersion = async function ({
-  versionRange,
-  preferredNodeOpts,
-  nodeVersionAliasOpts,
-}) {
-  if (versionRange !== 'now') {
-    return nodeVersionAlias(versionRange, nodeVersionAliasOpts)
-  }
-
-  const { version } = await preferredNodeVersion(preferredNodeOpts)
-
-  if (version === undefined) {
-    return processVersion.replace('v', '')
-  }
-
-  return version
-}
-
-// Node <0.8.6 only shipped source code for Unix. We don't want to support
-// building from sources, so we can't support those very old versions.
-const checkVersion = function (version) {
-  if (ltVersion(version, MINIMUM_VERSION)) {
-    throw new Error(
-      `Unsupported Node.js version: ${version}. Must be >= 0.8.6.`,
-    )
-  }
-}
-
-const MINIMUM_VERSION = '0.8.6'
 
 // We do not use `export default` because Babel transpiles it in a way that
 // requires CommonJS users to `require(...).default` instead of `require(...)`.
---
+++ src/version.js
@@ -0,0 +1,53 @@
+import { version as processVersion } from 'process'
+
+import nodeVersionAlias from 'node-version-alias'
+import preferredNodeVersion from 'preferred-node-version'
+import { lt as ltVersion } from 'semver'
+
+// Resolve full Node.js version.
+// We resolve aliases like 'latest' or 'lts' using `node-version-alias`.
+// The 'now' alias uses more complex logic using `preferred-node-version` and
+// defaults to `process.version`.
+export const getVersion = async function ({
+  versionRange,
+  preferredNodeOpts,
+  nodeVersionAliasOpts,
+}) {
+  const version = await resolveVersion({
+    versionRange,
+    preferredNodeOpts,
+    nodeVersionAliasOpts,
+  })
+  checkVersion(version)
+  return version
+}
+
+const resolveVersion = async function ({
+  versionRange,
+  preferredNodeOpts,
+  nodeVersionAliasOpts,
+}) {
+  if (versionRange !== 'now') {
+    return nodeVersionAlias(versionRange, nodeVersionAliasOpts)
+  }
+
+  const { version } = await preferredNodeVersion(preferredNodeOpts)
+
+  if (version === undefined) {
+    return processVersion.replace('v', '')
+  }
+
+  return version
+}
+
+// Node <0.8.6 only shipped source code for Unix. We don't want to support
+// building from sources, so we can't support those very old versions.
+const checkVersion = function (version) {
+  if (ltVersion(version, MINIMUM_VERSION)) {
+    throw new Error(
+      `Unsupported Node.js version: ${version}. Must be >= ${MINIMUM_VERSION}.`,
+    )
+  }
+}
+
+const MINIMUM_VERSION = '0.8.6'

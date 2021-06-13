b19b8289f62f121969b231b6e6ca38c267a10a47
ehmicky <ehmicky@gmail.com>
2021-06-13 11:54:05 +0200

    Use default export

--- src/main.js
+++ src/main.js
@@ -3,7 +3,8 @@ import { getOpts } from './options.js'
 import { getVersion } from './version.js'
 
 // Download the Node.js binary for a specific `versionRange`
-const getNode = async function (versionRange, opts) {
+// eslint-disable-next-line import/no-default-export
+export default async function getNode(versionRange, opts) {
   const {
     versionRange: versionRangeA,
     output,
@@ -21,7 +22,3 @@ const getNode = async function (versionRange, opts) {
   const nodePath = await download({ version, output, arch, fetchOpts })
   return { version, path: nodePath }
 }
-
-// We do not use `export default` because Babel transpiles it in a way that
-// requires CommonJS users to `require(...).default` instead of `require(...)`.
-module.exports = getNode
